db2bb3df7ca01b926b36487ee03a6e61e2d07fa5
Ramon Victor Cardoso <ramonvc19@gmail.com>
2022-07-27 15:25:13 -0300

    Initial commit

---
+++ exercicio1/solucao.js
@@ -0,0 +1,18 @@
+function definirLivro(nome, ano, autor) {
+    ano_atual = new Date().getFullYear();
+
+    const nome_maisculo = nome.toUpperCase();
+    const idade = ano_atual - ano;
+    const por = nome + ' por ' + autor;
+
+    return {
+        nome: nome_maisculo,
+        idade,
+        por
+    }
+
+}
+
+
+let livro = definirLivro('O Senhor dos Anéis', 1954, 'J. R. R. Tolkien');
+console.log(livro);
