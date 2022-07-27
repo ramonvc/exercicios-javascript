570e6ce54ce1e2edb2089251b064ca1c3e210af5
Ramon Victor Cardoso <ramonvc19@gmail.com>
2022-07-27 16:55:02 -0300

    nova função com desempenho melhorado

--- exercicio2/solucao.js
+++ exercicio2/solucao.js
@@ -7,4 +7,11 @@ function seguirMouse(event) {
 }
 
 
+const circulo = document.querySelector('#circulo');
+
+function seguirMouse2(event) {
+    circulo.style.top = event.y + "px";
+    circulo.style.left = event.x + "px";
+}
+
 window.addEventListener('mousemove', seguirMouse)
